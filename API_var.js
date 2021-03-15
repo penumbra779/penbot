c820caa27c3673af124095ec37dc7cb7075ac63b
zachary <zacharywies@gmail.com>
2021-03-15 01:40:53 -0400

    monke

---
+++ API_var.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+//twitter
+console.log('wHpuhjkQ8v9cBOaHfv2GcgUCQ', process.env.TWITTER_CONSUMER_KEY);
+console.log('660GQ57U9Rv8AJTxTA6rFTMOnpGxW9Es7C5INRkWSWCbpC1xfT', consumer_secret: process.env.TWITTER_CONSUMER_SECRET);
+console.log('AAAAAAAAAAAAAAAAAAAAABGhNgEAAAAAimRdFXwnT%2BdfVi1sglEb%2FAr%2FBeo%3DgwVak4FaxsKJhspTlTwaYZhrkgndS0guHVpsKmF1R4HJjxihUV', bearer_token: process.env.TWITTER_BEARER_TOKEN);
