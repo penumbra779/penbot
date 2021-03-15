50c610fe67c298edcbed94cc531f4aae2d11c6e6
David <dlruddell@gmail.com>
2021-03-14 21:51:40 -0400

    Cleanup and add assets and templates base

---
+++ yahoo_test.js
@@ -0,0 +1,27 @@
+var fs = require("fs");
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://yahoo-finance-low-latency.p.rapidapi.com/v8/finance/chart/AAPL");
+
+req.query({
+	"comparisons": "MSFT,^VIX",
+	"events": "div,split"
+});
+
+req.headers({
+	"x-rapidapi-key": "f4cab2666cmshe38db34a385b800p1f5848jsn180c63f03618",
+	"x-rapidapi-host": "yahoo-finance-low-latency.p.rapidapi.com",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+  fs.writeFile("yahoo_test.json", JSON.stringify(res, null, 2), function(err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+	console.log(res.body);
+});
